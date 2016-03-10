0bb98754d53b596e66b263baa63c1259be539538
javimuu <muuduy@gmail.com>
2016-03-10 15:40:06 +0700

    create templates, create model

---
+++ brs/postgres.py
@@ -0,0 +1,12 @@
+__author__ = 'javimuu'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'brs',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}

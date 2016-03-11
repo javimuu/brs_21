5fd7e77157b4709fb547ac9a06699fdbc192cb33
quynv <quytnhanam@gmail.com>
2016-03-11 12:28:30 +0700

    Setting

---
+++ brs/settings/dev.py
@@ -0,0 +1,12 @@
+from brs.settings.base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'brs',
+        'USER': 'quynv',
+        'PASSWORD': 'quynv@123',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
